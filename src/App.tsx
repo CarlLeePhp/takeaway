ec3f58f83c22c271bc33c59197b7b2e0291ea169
Carl Li <leekunhui@sina.cn>
2023-04-17 09:49:25 +1200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Category } from "./Category";
 
 function App() {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:7065/api/categories")
+      .then((response) => response.json())
+      .then((data) => setCategories(data));
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Categories List</h1>
+      <ul>
+        {categories.map((category, index) => (
+          <li key={index}>
+            {category.id} - {category.description}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
